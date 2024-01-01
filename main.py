a62730c0d25b0e292dddb67257644194b4274c33
= <damodaranejb@gmail.com>
2024-01-01 23:30:30 +0100

    estimated every functions in the FacialAnalysisProcessor class

--- main.py
+++ main.py
@@ -1,29 +1,4 @@
-import numpy as np
-import dlib
-import cv2
+from FacialAnalysisProcessor import FacialAnalysisProcessor
+import time
 
-class FacialFilterApplier:
-    def __init__(self):
-        self
-
-    # to apply the filters
-
-    def detect_faces(self):
-        return
-    
-    def apply_filters_to_faces(self):
-        return
-
-    def executeFilter(self):
-        return
-    
-    # converts face landmarks into an array
-    
-    def landmarks_to_tuples(self):
-        return
-
-    def convert_shape_to_array(self):
-        return
-
-    def process_face_with_filters(self):
-        return 
+FacialAnalysisProcessor()
